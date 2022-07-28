095e314c778c4ed9afc4cd4ffd5f054714a31800
Korben Dallas <pvsaintpe@icloud.com>
2022-07-28 11:36:03 +0300

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 psql postgres -U user -tc "SELECT 1 FROM pg_database WHERE datname = 'testing'" | grep -q 1 || psql postgres -U user -c "CREATE DATABASE testing"
 cd app
 rm -f .env

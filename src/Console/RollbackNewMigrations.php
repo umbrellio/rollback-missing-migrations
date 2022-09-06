37905e873bb0b66b7a4756548d57516cec307d8e
kirill.osa <kirill.osa@okwork.io>
2022-09-06 11:31:23 +0300

    fix old rollback

--- src/Console/RollbackNewMigrations.php
+++ src/Console/RollbackNewMigrations.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace Umbrellio\Deploy\Console;
 
 use Illuminate\Console\Command;
+use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 use SplFileInfo;
 
 class RollbackNewMigrations extends Command
@@ -56,6 +58,10 @@ class RollbackNewMigrations extends Command
     {
         $migration = require_once $f;
         $filename = explode('.php', $f->getRelativePathname())[0];
+        if (!$migration instanceof Migration) {
+            $class = Str::studly(implode('_', array_slice(explode('_', $filename), 4)));
+            $migration = new $class();
+        }
         if (method_exists($migration, 'down')) {
             $migration->down();
             DB::table(config('database.migrations', 'migrations'))->where('migration', $filename)->delete();
